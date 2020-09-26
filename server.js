06e3367e57edee3c8f762882bbd692834ea83e69
anwarcode <21023424+anwarcode@users.noreply.github.com>
2020-09-27 00:24:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -47,4 +47,4 @@ client.on("guildMemberAdd", member =>
 require("./reactions.js").run(client);
 
 // Login Discord Bot
-client.login("NzA5MTM3MjczOTc4ODgwMDEy.Xt_eZA.KuLcXXLWOeEfz1C-9kzhNlNt_-0");
+client.login(process.env.token);
